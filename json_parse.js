36a0dcabb8f498adb9ff2d414582bcc8b2b1a25a
Pranoti9916 <71695567+Pranoti9916@users.noreply.github.com>
2020-11-04 11:29:00 +0530

    Updated Code

--- json_parse.js
+++ json_parse.js
@@ -38,16 +38,17 @@ function findDay(date)
     return weekday[d.getDay()]
 }
 
- function findProductQuantity(productId,productQuantity={
-     "MONDAY" : 0,
+ function findProductQuantity(productId)
+ {
+ let productQuantity={"MONDAY" : 0,
      "TUESDAY" : 0,
      "WEDNESDAY" : 0,
      "THURSDAY" : 0,
      "FRIDAY " : 0,
      "SATURDAY" : 0,
-     "SUNDAY" : 0
- })
- {
+     "SUNDAY" : 0}
+ 
+ 
       json_input.forEach(input => {
           let day=findDay(input.creationDate)
           let store =input.orderLines.find(arrayInput=>arrayInput.productId===productId)
